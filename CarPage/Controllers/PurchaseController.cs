e06558069b42bcb8dd89398acfd984d75e553a63
tunaparlak0 <tuna.parlak@ogr.sakarya.edu.tr>
2024-12-17 10:20:08 +0300

    v4

--- CarPage/Controllers/PurchaseController.cs
+++ CarPage/Controllers/PurchaseController.cs
@@ -1,11 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using CarPage.Models;
+using System.Linq;
 
 namespace CarPage.Controllers
 {
     public class PurchaseController : Controller
     {
-        public IActionResult Index()
+        private readonly CarContext _context;
+
+        public PurchaseController(CarContext context)
         {
+            _context = context;
+        }
+
+        public IActionResult Index(int carId)
+        {
+            var car = _context.Cars.FirstOrDefault(c => c.Id == carId);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return View(car);
+        }
+        [HttpPost]
+        public IActionResult Message()
+        {
+            ViewBag.Message = "Your purchase request has been submitted successfully! We will contact you about payment details. Thanks for choosing us!";
             return View();
         }
     }
--- CarPage/Models/Purchase.cs
+++ CarPage/Models/Purchase.cs
@@ -3,15 +3,15 @@ using System.ComponentModel.DataAnnotations;
 public class Purchase
 {
     [Required]
-    public string Name { get; set; } = string.Empty;
+    public string? Name { get; set; }
 
     [Required]
-    public string Address { get; set; } = string.Empty;
+    public string? Address { get; set; }
 
     [Required]
     [EmailAddress]
-    public string Email { get; set; } = string.Empty;
+    public string? Email { get; set; } 
 
     [Required]
-    public string Phone { get; set; } = string.Empty;
+    public string? Phone { get; set; }
 }
